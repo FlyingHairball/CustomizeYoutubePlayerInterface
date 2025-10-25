7b037a3b55299cec613dee9d5fd5b0616c501f50
FlyingHairball <flyinghairball@hotmail.com>
2025-10-25 14:59:28 +0300

    firefox builds

--- Background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener((msg, _sender, sendResponse) => {
-    if (msg.type === "getData") {
-        chrome.storage.local.get(null, (data) => {
-            sendResponse({ data });
-        });
-        return true;
-    }
-});
